6aad3c7b6eb319ba58344d7844553ca19e2475d4
nerius.perez <ntoirac@proveedor.teco.com.ar>
2020-06-25 11:34:49 -0300

    Add validators and sanitzation

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -31,47 +31,64 @@ const User = mongoose.model('User', {
                 throw new Error('The mail is not correctly')
             }
         }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim:true,
+        minlength: 6,
+        validate(value) {
+            if(value.toUpperCase().includes('PASSWORD')) {
+                throw new Error('The passwor do not should contain the word "passwor", either uppercase or lowercase!!!')
+            }
+        }
     }
 })
 
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        trim: true,
+        required:true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        required: false,
+        default: false
     }
 })
 
-/* const task = new Task({
-    description: 'Develop my first app with nodejs',
-    completed: false
+const task = new Task({
+    description: ' Learn Enlish  ',
+    completed: true
 })
 
 task.save().then((result) => {
     console.log('Task add')
 }).catch((error) => {
     console.log('Error ocurred was saving the task!!!', error)
-}) */
-const user = new User({
+})
+/* const user = new User({
     age: 37,
-    name: '   Nerius   ',
-    email: 'Suirennerius1982@gmAil.com'
+    name: '   Hacker   ',
+    email: 'Suirennerius1982@gmAil.com',
+    password: '   sdfgfPASsWOr   ggggf '
 })
 
 user.save().then((result) => {
     console.log(result)
 }).catch((error) => {
     console.log(error)
-})
+}) */
 
-const user2 = new User({
+/* const user2 = new User({
     name: '   sUIREN   ',
-    email: 'Suirennerius1982@YAHOO.com   '
+    email: 'Suirennerius1982@YAHOO.com   ',
+    password: 'localpasswordhots'
 })
 
 user2.save().then((result) => {
     console.log(result)
 }).catch((error) => {
     console.log(error)
-})
+}) */
