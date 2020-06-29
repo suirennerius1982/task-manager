e3e325e3fbbf0714a93d493bc6952325566b55c4
nerius.perez <ntoirac@proveedor.teco.com.ar>
2020-06-29 12:35:32 -0300

    example encrypted library with nodejs

--- src/index.js
+++ src/index.js
@@ -15,6 +15,21 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
+const bcrypt = require('bcryptjs')
+
+const myFuction = async (apsw) => {
+    const myPsw = 'nerius21!'
+    const encryptNerius = await bcrypt.hash(apsw, 8)
+    const encryptPsw = await bcrypt.hash(myPsw, 8)
+    console.log(myPsw)
+    console.log(encryptPsw)
+    console.log(encryptNerius)
+    const isEqualPsw = await bcrypt.compare(apsw, encryptNerius)
+    console.log(isEqualPsw?'true':'false')
+}
+
+myFuction('nerius21!')
+
 app.listen(port, () => {
     console.log(`Server is up on port: ${port}`)
 })
