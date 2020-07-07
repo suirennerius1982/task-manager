2dfaaa5df943aa6aed896743a0ce1749235394b7
nerius.perez <ntoirac@proveedor.teco.com.ar>
2020-07-07 17:57:43 -0300

    Add cancelation subcription fot the user

--- src/emails/acount.js
+++ src/emails/acount.js
@@ -4,9 +4,31 @@ const sendgridAPIKey = 'SG.hwoED2d6TxmtA-XkthqXmw.UB7-tM2Bpuk1CnMDGO2Rr0PWbwSO4D
 
 sgMail.setApiKey(sendgridAPIKey)
 
-sgMail.send({
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'suirennerius1982@gmail.com',
+        subject: 'Welcome to Task Manager!!!',
+        text: `Welcome to the app, ${name} lest me know how you get along with the app.`
+    })
+}
+
+const cancelSubcription = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'suirennerius1982@gmail.com',
+        subject: 'Why did you cancel the subcription. Please, should be comment to respect?',
+        text: `Up ${name}, we will desire you come back someday!!! You are welcome!!!`
+    })
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    cancelSubcription
+}
+/* sgMail.send({
     to: 'digitalbuildonline@gmail.com',
     from: 'suirennerius1982@gmail.com',
     subject: 'This is my first creation! :)',
     text: 'I hope this one actually get you'
-})
+}) */
--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,7 @@ const User = require('../models/user')
 const auth = require('../middleware/auth')
 const multer = require('multer')
 const sharp = require('sharp')
+const { sendWelcomeEmail, cancelSubcription } = require('../emails/acount')
 
 const router = new express.Router()
 
@@ -45,6 +46,7 @@ router.post('/users', async (req, res) => {
     const user = new User(req.body)
     try {
         await user.save()
+        sendWelcomeEmail(user.email, user.name)
         const token = await user.getToken()
         //res.status(201).send({user: user.getPublicProfile(), token})
         res.status(201).send({ user, token })
@@ -128,6 +130,7 @@ router.post('/user/me/avatar', auth, upload.single('avatar'), async (req, res) =
 router.delete('/users/me', auth, async (req, res) => {
     try {
         await req.user.remove()
+        cancelSubcription(req.user.email, req.user.name)
         res.send(req.user)
     } catch (error) {
         res.status(500).send({ error: error.message })
