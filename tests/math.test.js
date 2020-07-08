c3e22da1d608a0bdbc82116fe5c04e48b5800fce
nerius.perez <ntoirac@proveedor.teco.com.ar>
2020-07-08 16:00:28 -0300

    Examples and challenge using the jest

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,7 +1,29 @@
-test('Hello word', () => {
+const {calculateTip, fahrenheitToCelsius, celsiusToFahrenheit} = require('../src/math')
 
+test('Should calculate total with tip', () => {
+    const total = calculateTip(10, .3)
+    expect(total).toBe(13)
+    /* if (total !== 13) {
+        throw new Error(`The total value calculate is incorrectly. The value should be 13 and recibed ${total}`)
+    } */
 })
 
-test('Test failure', () => {
-    throw new Error('Test failure!!!')
+//
+// Goal: Test temperature conversion functions
+//
+// 1. Export both functions and load them into test suite
+// 2. Create "Should convert 32 F to 0 C"
+// 3. Create "Should convert 0 C to 32 F"
+// 4. Run the Jest to test your work!
+
+test('Should convert 32 F to 0 C', () => {
+    expect(fahrenheitToCelsius(32)).toBe(0)
+})
+
+test('Should convert 0 C to 32 F', () => {
+    expect(celsiusToFahrenheit(0)).toBe(32)
+})
+
+test('Should calculate total with default tip', () => {
+    expect(calculateTip(10)).toBe(12.5)
 })
