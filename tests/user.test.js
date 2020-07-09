442ab06e5df3b0ef2656d78322a7fd136802f968
nerius.perez <ntoirac@proveedor.teco.com.ar>
2020-07-09 07:58:42 -0300

    Test case using the beforEach

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,18 @@
 const request = require('supertest')
 const app = require('../src/app')
+const user = require('../src/models/user')
+const User = require('../src/models/user')
+
+const userOne = {
+    name: 'Digital Build Online',
+    email: 'digitalbuildonline@gmail.com',
+    password: 'prueba123'
+}
+
+beforeEach(async () => {
+    await User.deleteMany()
+    await new User(userOne).save()
+})
 
 test('Should sinup new user', async () => {
     await request(app).post('/users').send({
@@ -7,4 +20,19 @@ test('Should sinup new user', async () => {
         email: 'suirennerius1982@gmail.com',
         password: 'prueba123'
     }).expect(201)
+})
+
+test('Should login to app', async () => {
+    await request(app).post('/users/login').send({
+        name: userOne.name,
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
+})
+
+test('Should logout the user', async () => {
+    await request(app).post('/users/logout').send({
+        email: 'testloginfail@gmail.com',
+        password: 'prueba123'
+    }).expect(401)
 })
