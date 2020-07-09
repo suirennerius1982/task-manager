ccd4b7860c1d5758fda1a196c697fa760cc6ca5a
nerius.perez <ntoirac@proveedor.teco.com.ar>
2020-07-09 11:07:49 -0300

    Run examplet about assert test from endpoints with and without authorization

--- tests/user.test.js
+++ tests/user.test.js
@@ -2,11 +2,20 @@ const request = require('supertest')
 const app = require('../src/app')
 const user = require('../src/models/user')
 const User = require('../src/models/user')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 
+const userOneId = new mongoose.Types.ObjectId()
 const userOne = {
+    _id: userOneId,
     name: 'Digital Build Online',
     email: 'digitalbuildonline@gmail.com',
-    password: 'prueba123'
+    password: 'prueba123',
+    tokens: [
+        {
+            token: jwt.sign({_id: userOneId}, process.env.JWT_SECRET)
+        }
+    ]
 }
 
 beforeEach(async () => {
@@ -35,4 +44,41 @@ test('Should logout the user', async () => {
         email: 'testloginfail@gmail.com',
         password: 'prueba123'
     }).expect(401)
+})
+
+test('Should login sussefull', async () => {
+    await request(app)
+                .post('/users/login')
+                .set('Authorization', userOne.tokens[0])
+                .send({
+                    email: userOne.email,
+                    password: userOne.password
+                })
+                .expect(200)
+})
+
+test('Should not login because header token is not provider', async () => {
+    await request(app)
+                .get('/users/me')
+                .send({
+                    email: userOne.email,
+                    password: userOne.password
+                })
+                .expect(401)
+})
+
+test('Should delete acount of user', async () => {
+    //console.log(userOne.tokens[0])
+    await request(app)
+                .delete('/users/me')
+                .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+                .send()
+                .expect(200)
+})
+
+test('Should not delete acount for unauthenticated user', async () => {
+    await request(app)
+                .delete('/users/me')
+                .send()
+                .expect(401)
 })
